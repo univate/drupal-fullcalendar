7d9bbf227c8ecf6b5a434fb8ee039ca5949ce3ab
Chris Hood <chris@univate.com.au>
2010-08-17 11:58:42 +1000

    Inital version, creates the calendar, but doesn't allow manipulation of data from drupal

---
+++ README.txt
@@ -0,0 +1,9 @@
+fullcalendar
+~~~~~~~~~~~~
+
+Allows the creation of google spreadsheet type feature in drupal as a views plugin. This is done with the jquery fullcalendar (http://arshaw.com/fullcalendar/).
+
+
+Developers:
+~~~~~~~~~~~
+Chris Hood - http://univate.com.au
---
+++ fullcalendar-view.tpl.php
@@ -0,0 +1,13 @@
+<div class="item-list">
+</div>
+
+
+<div class="external-events">
+  <?php if (!empty($title)) : ?>
+    <h3><?php print $title; ?></h3>
+  <?php endif; ?>
+  <?php foreach ($rows as $id => $row): ?>
+    <div class="external-event <?php print $classes[$id]; ?>"><?php print $row; ?></div>
+  <?php endforeach; ?>
+</div>
+<div id="fullcalendar_<?php print $id; ?>" class="fullcalendar"></div>
